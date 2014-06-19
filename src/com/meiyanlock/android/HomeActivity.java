2e030fd1d0ba39b1fab2e853636ae47ac2decb44
ToYond <atlantisy@qq.com>
2014-06-20 00:04:37 +0800

    新增九宫格文字view

--- src/com/meiyanlock/android/HomeActivity.java
+++ src/com/meiyanlock/android/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.meiyanlock.android;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,8 +10,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.SimpleAdapter;
 
 public class HomeActivity extends Activity {
 
@@ -16,20 +23,38 @@ public class HomeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-		
-		//设置按钮
-		ImageButton setting_button = (ImageButton)findViewById(R.id.home_setting);
+
+		// 九宫格视图
+		GridView verse_gridview = (GridView) findViewById(R.id.grid_verse);
+		// 生成动态数组，并且转入数据
+		ArrayList<HashMap<String, Object>> listVerse = new ArrayList<HashMap<String, Object>>();
+		for (int i = 1; i < 10; i++) {
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("Verse", "" + String.valueOf(i));
+			listVerse.add(map);
+		}
+		// 生成适配器的Verses <====> 动态数组的元素，一一对应
+		SimpleAdapter saVerses = new SimpleAdapter(this, listVerse,
+				R.layout.grid_verse, new String[] { "Verse" },
+				new int[] { R.id.verse_content });
+		// 添加并且显示
+		verse_gridview.setAdapter(saVerses);
+		// 添加消息处理
+		verse_gridview.setOnItemClickListener(new ItemClickListener());
+
+		// 设置按钮
+		ImageButton setting_button = (ImageButton) findViewById(R.id.home_setting);
 		setting_button.setOnClickListener(settingOnClickListener);
-		
-		//编辑美言按钮
-		ImageButton text_button = (ImageButton)findViewById(R.id.home_text);
+
+		// 编辑美言按钮
+		ImageButton text_button = (ImageButton) findViewById(R.id.home_text);
 		text_button.setOnClickListener(textOnClickListener);
-		
-		//切换锁屏方式按钮
-		final ImageButton lock_line_button = (ImageButton)findViewById(R.id.home_lock_line);
-		final ImageButton lock_grid_button = (ImageButton)findViewById(R.id.home_lock_grid);
-				
-		lock_line_button.setOnClickListener(new OnClickListener() {			
+
+		// 切换锁屏方式按钮
+		final ImageButton lock_line_button = (ImageButton) findViewById(R.id.home_lock_line);
+		final ImageButton lock_grid_button = (ImageButton) findViewById(R.id.home_lock_grid);
+		// 简约锁屏事件
+		lock_line_button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
@@ -37,8 +62,8 @@ public class HomeActivity extends Activity {
 				lock_grid_button.setVisibility(View.VISIBLE);
 			}
 		});
-		
-		lock_grid_button.setOnClickListener(new OnClickListener() {			
+		// 九宫格锁屏事件
+		lock_grid_button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
@@ -48,24 +73,41 @@ public class HomeActivity extends Activity {
 		});
 	}
 
-	//设置按钮点击事件
+	// 当AdapterView被单击(触摸屏或者键盘)，则返回的Item单击事件
+	class ItemClickListener implements OnItemClickListener {
+		public void onItemClick(AdapterView<?> arg0,// The AdapterView where the
+													// click happened
+				View arg1,// The view within the AdapterView that was clicked
+				int arg2,// The position of the view in the adapter
+				long arg3// The row id of the item that was clicked
+		) {
+			// 在本例中arg2=arg3
+			HashMap<String, Object> item = (HashMap<String, Object>) arg0
+					.getItemAtPosition(arg2);
+			// 显示所选Item的ItemText
+			setTitle((String) item.get("Verse"));
+		}
+
+	}
+
+	// 设置按钮点击事件
 	private OnClickListener settingOnClickListener = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View arg0) {
 			// TODO Auto-generated method stub
-			startActivity(new Intent(HomeActivity.this, SettingActivity.class));  
+			startActivity(new Intent(HomeActivity.this, SettingActivity.class));
 		}
 	};
-	
-	//编辑美言按钮点击事件
+
+	// 编辑美言按钮点击事件
 	private OnClickListener textOnClickListener = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View arg0) {
 			// TODO Auto-generated method stub
-			startActivity(new Intent(HomeActivity.this, TextEditActivity.class));  
+			startActivity(new Intent(HomeActivity.this, TextEditActivity.class));
 		}
 	};
-	
+
 }
