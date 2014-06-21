7b44bfe45f22c732b623ded78f7e5c98a84ad094
ToYond <atlantisy@qq.com>
2014-06-22 03:28:34 +0800

    九宫格手势锁屏模式初始化汉字字符

--- src/com/meiyanlock/android/HomeActivity.java
+++ src/com/meiyanlock/android/HomeActivity.java
@@ -3,6 +3,8 @@ package com.meiyanlock.android;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,11 +19,14 @@ import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeActivity extends Activity {
+	private TextView verse_line = null;
+	private GridView verse_grid = null;
 	private ImageButton lockbtn = null;// 锁屏按钮
-
+	
 	private static final int LINE = 1;// 简约状态
 	private static final int GRID = 2;// 九宫状态
 	private static final int STATE_LINE = 1;// 锁屏状态设为1
@@ -33,13 +38,25 @@ public class HomeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 
+		// 设置按钮
+		ImageButton setting_button = (ImageButton) findViewById(R.id.home_setting);
+		setting_button.setOnClickListener(settingOnClickListener);
+		// 编辑美言按钮
+		ImageButton text_button = (ImageButton) findViewById(R.id.home_text);
+		text_button.setOnClickListener(textOnClickListener);
+		
+		// Verse
+		String strVerse = "你是我的小呀小苹果";
+		// 简约视图
+		verse_line = (TextView) findViewById(R.id.line_verse);
 		// 九宫格视图
-		GridView verse_gridview = (GridView) findViewById(R.id.grid_verse);
+		verse_grid = (GridView) findViewById(R.id.grid_verse);		
+		char[] chVerse = strVerse.toCharArray();
 		// 生成动态数组，并且转入数据
 		ArrayList<HashMap<String, Object>> listVerse = new ArrayList<HashMap<String, Object>>();
-		for (int i = 1; i < 10; i++) {
+		for (int i = 0; i < 9; i++) {
 			HashMap<String, Object> map = new HashMap<String, Object>();
-			map.put("Verse", "" + String.valueOf(i));
+			map.put("Verse", "" + chVerse[i]);
 			listVerse.add(map);
 		}
 		// 生成适配器的Verses <====> 动态数组的元素，一一对应
@@ -47,19 +64,21 @@ public class HomeActivity extends Activity {
 				R.layout.grid_verse, new String[] { "Verse" },
 				new int[] { R.id.verse_content });
 		// 添加并且显示
-		verse_gridview.setAdapter(saVerses);
+		verse_grid.setAdapter(saVerses);
 		// 添加消息处理
-		verse_gridview.setOnItemClickListener(new ItemClickListener());
-
-		// 设置按钮
-		ImageButton setting_button = (ImageButton) findViewById(R.id.home_setting);
-		setting_button.setOnClickListener(settingOnClickListener);
-
-		// 编辑美言按钮
-		ImageButton text_button = (ImageButton) findViewById(R.id.home_text);
-		text_button.setOnClickListener(textOnClickListener);
+		verse_grid.setOnItemClickListener(new ItemClickListener());
 
 		// 切换锁屏方式
+		switch (flag) {
+		case STATE_LINE:
+			verse_line.setVisibility(View.VISIBLE);
+			verse_grid.setVisibility(View.GONE);			
+			break;			
+		case STATE_GRID:
+			verse_line.setVisibility(View.GONE);
+			verse_grid.setVisibility(View.VISIBLE);
+			break;
+		}
 		ShowLockBtn();
 
 	}
@@ -90,16 +109,20 @@ public class HomeActivity extends Activity {
 	protected void line() {
 		flag = LINE;
 		lockbtn.setImageResource(R.drawable.lock_grid);
-		Toast.makeText(getApplicationContext(), "简单滑动锁屏",
-			     Toast.LENGTH_SHORT).show();
+		verse_line.setVisibility(View.VISIBLE);
+		verse_grid.setVisibility(View.GONE);
+        Toast.makeText(this, R.string.line_verse_style, Toast.LENGTH_LONG)
+        .show();
 	}
 
 	// 九宫锁屏
 	protected void grid() {
 		flag = GRID;
 		lockbtn.setImageResource(R.drawable.lock_line);
-		Toast.makeText(getApplicationContext(), "九宫滑动锁屏",
-			     Toast.LENGTH_SHORT).show();
+		verse_line.setVisibility(View.GONE);
+		verse_grid.setVisibility(View.VISIBLE);
+        Toast.makeText(this, R.string.grid_verse_style, Toast.LENGTH_LONG)
+        .show();
 	}
 
 	// 当AdapterView被单击(触摸屏或者键盘)，则返回的Item单击事件
