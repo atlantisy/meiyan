66b30efc090a3fd1c4eabf651e213ff149b0c887
ToYond <atlantisy@qq.com>
2014-07-11 02:50:53 +0800

    将home的九宫gridview改为9个textview，提高视觉体验

--- src/com/meiyanlock/android/HomeActivity.java
+++ src/com/meiyanlock/android/HomeActivity.java
@@ -2,10 +2,15 @@ package com.meiyanlock.android;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshGridView;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener2;
 import com.meiyanlock.android.R;
 import com.meiyanlock.widget.CustemSpinerAdapter;
+import com.meiyanlock.widget.LocusPassWordView;
 import com.meiyanlock.widget.SpinerPopWindow;
 import com.meiyanlock.widget.CustemObject;
 import com.meiyanlock.widget.AbstractSpinerAdapter;
@@ -22,9 +27,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,7 +39,11 @@ import android.widget.Toast;
 public class HomeActivity extends Activity implements OnClickListener, AbstractSpinerAdapter.IOnItemSelectListener{
 	private TextView verse_line = null;
 	private GridView verse_grid = null;
+	private LinearLayout verse_grid1 = null;
+	
+	//private LocusPassWordView verse_grid = null;
 	private ImageButton lockbtn = null;// 锁屏按钮
+	private Button setup_grid_button = null;//设置九宫手势按钮
 	
 	private Button mBtnDropDown;
 	private List<CustemObject> nameList = new ArrayList<CustemObject>();
@@ -43,15 +54,46 @@ public class HomeActivity extends Activity implements OnClickListener, AbstractS
 	private static final int STATE_LINE = 1;// 锁屏状态设为1
 	private static final int STATE_GRID = 2;// 锁屏状态设为2
 	private int flag = 1;// 标记
+	
+	
+	static final int MENU_SET_MODE = 0;
+
+	private PullToRefreshGridView mPullRefreshGridView;
+	private GridView mGridView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 
-		// 美言种类选择
-		verseOptionSetup();
+		//下拉刷新
+		mPullRefreshGridView = (PullToRefreshGridView) findViewById(R.id.pull_refresh_grid);
+		mGridView = mPullRefreshGridView.getRefreshableView();
+		// Set a listener to be invoked when the list should be refreshed.
+		mPullRefreshGridView.setOnRefreshListener(new OnRefreshListener2<GridView>() {
+
+			@Override
+			public void onPullDownToRefresh(PullToRefreshBase<GridView> refreshView) {
+//				mPullRefreshGridView.getLoadingLayoutProxy().setRefreshingLabel("正在加载"); 
+//				mPullRefreshGridView.getLoadingLayoutProxy().setPullLabel("下拉推荐美言…"); 
+//				mPullRefreshGridView.getLoadingLayoutProxy().setReleaseLabel("放开以推荐…");
+				//下拉
+				PullDown();
+			}
+
+			@Override
+			public void onPullUpToRefresh(PullToRefreshBase<GridView> refreshView) {
+/*				mPullRefreshGridView.getLoadingLayoutProxy().setRefreshingLabel("正在加载"); 
+				mPullRefreshGridView.getLoadingLayoutProxy().setPullLabel("上拉编辑美言"); 
+				mPullRefreshGridView.getLoadingLayoutProxy().setReleaseLabel("放开以编辑");*/
+				//上拉
+				PullUp();
+			}
+
+		});
 		
+		//美言类型设置选项
+		verseOptionSetup();		
 		// 设置按钮
 		ImageButton setting_button = (ImageButton) findViewById(R.id.home_setting);
 		setting_button.setOnClickListener(settingOnClickListener);
@@ -59,11 +101,17 @@ public class HomeActivity extends Activity implements OnClickListener, AbstractS
 		ImageButton text_button = (ImageButton) findViewById(R.id.home_text);
 		text_button.setOnClickListener(textOnClickListener);
 		
+		// 设置九宫格手势
+		setup_grid_button = (Button) findViewById(R.id.home_setup_grid);
+		setup_grid_button.setOnClickListener(setGridOnClickListener);
+		
 		// Verse
-		String strVerse = "你是我的小呀小苹果";
+		String strVerse = "感觉 自己 萌萌哒";
 		// 简约视图
 		verse_line = (TextView) findViewById(R.id.line_verse);
 		// 九宫格视图
+		//verse_grid = (LocusPassWordView) findViewById(R.id.mLocusPassWordView);
+		verse_grid1 = (LinearLayout) findViewById(R.id.verse_layout);
 		verse_grid = (GridView) findViewById(R.id.grid_verse);		
 		char[] chVerse = strVerse.toCharArray();
 		// 生成动态数组，并且转入数据
@@ -86,18 +134,35 @@ public class HomeActivity extends Activity implements OnClickListener, AbstractS
 		switch (flag) {
 		case STATE_LINE:
 			verse_line.setVisibility(View.VISIBLE);
-			verse_grid.setVisibility(View.GONE);			
+			verse_grid1.setVisibility(View.GONE);			
 			break;			
 		case STATE_GRID:
 			verse_line.setVisibility(View.GONE);
-			verse_grid.setVisibility(View.VISIBLE);
+			verse_grid1.setVisibility(View.VISIBLE);
 			break;
 		}
 		ShowLockBtn();
 
 	}
 
-    private void verseOptionSetup(){
+	//下拉
+	private void PullDown(){
+		// Call onRefreshComplete when the list has been refreshed.
+		mPullRefreshGridView.onRefreshComplete();
+		
+	}
+	
+	//上拉编辑
+	private void PullUp(){
+		//进入编辑模式		
+		startActivity(new Intent(HomeActivity.this, TextEditActivity.class));
+		// Call onRefreshComplete when the list has been refreshed.
+		mPullRefreshGridView.onRefreshComplete();
+		
+	}
+	
+    //美言类型设置选项
+	private void verseOptionSetup(){
 
 		mBtnDropDown = (Button) findViewById(R.id.verse_option);
 		mBtnDropDown.setOnClickListener(this);
@@ -145,7 +210,8 @@ public class HomeActivity extends Activity implements OnClickListener, AbstractS
 		flag = LINE;
 		lockbtn.setImageResource(R.drawable.ic_lock_grid);
 		verse_line.setVisibility(View.VISIBLE);
-		verse_grid.setVisibility(View.GONE);
+		verse_grid1.setVisibility(View.GONE);
+		setup_grid_button.setVisibility(View.GONE);
         Toast.makeText(this, R.string.line_verse_style, Toast.LENGTH_LONG)
         .show();
 	}
@@ -155,11 +221,12 @@ public class HomeActivity extends Activity implements OnClickListener, AbstractS
 		flag = GRID;
 		lockbtn.setImageResource(R.drawable.ic_lock_line);
 		verse_line.setVisibility(View.GONE);
-		verse_grid.setVisibility(View.VISIBLE);
+		verse_grid1.setVisibility(View.VISIBLE);
+		//setup_grid_button.setVisibility(View.VISIBLE);
         Toast.makeText(this, R.string.grid_verse_style, Toast.LENGTH_LONG)
         .show();
-        //设置九宫格手势
-        startActivity(new Intent(HomeActivity.this, SetPasswordActivity.class));
+/*        //设置九宫格手势
+        startActivity(new Intent(HomeActivity.this, SetPasswordActivity.class));*/
 	}
 
 	// 当AdapterView被单击(触摸屏或者键盘)，则返回的Item单击事件
@@ -196,6 +263,17 @@ public class HomeActivity extends Activity implements OnClickListener, AbstractS
 		public void onClick(View arg0) {
 			// TODO Auto-generated method stub
 			startActivity(new Intent(HomeActivity.this, TextEditActivity.class));
+		}
+	};
+	
+	// 设置九宫格按钮点击事件
+	private OnClickListener setGridOnClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View arg0) {
+			// TODO Auto-generated method stubsetGridOnClickListener
+	        //设置九宫格手势
+	        startActivity(new Intent(HomeActivity.this, SetPasswordActivity.class));
 		}
 	};
 	
